50179366c69a4f140e0eb5f6f2275c813fabe14b
Tuấn Trần <tuantransn2001@gmail.com>
2023-07-27 22:16:09 +0700

    fix missing build folder

---
+++ build/production/models/Staffagencybranchincharge.ts
@@ -0,0 +1,39 @@
+"use strict";
+import { Model } from "sequelize";
+import { StaffAgencyBranchInChargeAttributes } from "@/src/api/v2/ts/interfaces/app_interfaces";
+export default (sequelize: any, DataTypes: any) => {
+  class StaffAgencyBranchInCharge
+    extends Model<StaffAgencyBranchInChargeAttributes>
+    implements StaffAgencyBranchInChargeAttributes
+  {
+    id!: string;
+    staff_role_id!: string;
+    agency_branch_id!: string;
+    static associate({ StaffRole, AgencyBranch }: any) {
+      StaffAgencyBranchInCharge.belongsTo(StaffRole, {
+        foreignKey: "staff_role_id",
+        as: "Staff_Agency_Branch_InCharge",
+      });
+      StaffAgencyBranchInCharge.belongsTo(AgencyBranch, {
+        foreignKey: "agency_branch_id",
+      });
+    }
+  }
+  StaffAgencyBranchInCharge.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      staff_role_id: { type: DataTypes.UUID },
+      agency_branch_id: { type: DataTypes.UUID },
+    },
+    {
+      sequelize,
+      modelName: "StaffAgencyBranchInCharge",
+    }
+  );
+  return StaffAgencyBranchInCharge;
+};
