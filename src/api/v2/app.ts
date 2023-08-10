8b664ad97fd1414b35ec4598dcd7f96f020b98cd
Tuấn Trần <tuantransn2001@gmail.com>
2023-08-10 21:49:32 +0700

    complete v2 with spec - dev

--- src/api/v2/app.ts
+++ src/api/v2/app.ts
@@ -1,11 +1,13 @@
 require("dotenv").config();
-import express, { Express } from "express";
+import express, { Request, Response, Express } from "express";
 import cors from "cors";
 import env from "./constants/env";
 import rootRouter from "./routers";
 import APIGateWay from "./gateway/app.gateway";
 import db from "./models";
 import setupOnConnectDB from "./setup/setupOnConnectDB";
+import { STATUS_CODE, STATUS_MESSAGE } from "./ts/enums/api_enums";
+import RestFullAPI from "./utils/response/apiResponse";
 // ? ============================== INITIATE SERVER ==============================
 const app: Express = express();
 // ? ============================== VARIABLES ====================================
@@ -16,6 +18,18 @@ const HOST = env.host as string;
 app.use(cors()); // * Allow cors
 app.use(express.json()); //  * Converted Data into JSON type - !Important
 // ? ============================== HEALTH CHECK =================================
+app.get("/health", (_: Request, res: Response) => {
+  const data = {
+    uptime: process.uptime(),
+    message: "Ok",
+    date: new Date(),
+  };
+
+  res
+    .status(STATUS_CODE.STATUS_CODE_200)
+    .send(RestFullAPI.onSuccess(STATUS_MESSAGE.SUCCESS, data));
+});
+// ? ============================== USE ROUTER =================================
 app.use(ROOT_URL, APIGateWay.handleUseGlobalMiddleware, rootRouter); // * Use Router
 // ? ========================== CONNECT DATABASE - RUN SERVER ====================
 app.listen(PORT, async () => {
